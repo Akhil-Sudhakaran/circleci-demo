1341c38e4ba6e2e3da918e80fb21ddf7f70e976e
AkhilSudhakaran <akhil.sudhakaran@innovaturelabs.com>
2018-03-07 16:14:58 +0530

    deploy22

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,5 +1,5 @@
 tar -zcvf test.tar.gz src/
-scp test.tar.gz sokonokado@52.66.73.199:/home/sokonokado
+scp test.tar.gz sokonokado@52.66.73.199:/home/sokonokado/
 #
 # #Here you extract your archive on EC2 using SSH
 # ssh sokonokado@52.66.73.199 tar -xvzf test.tar.gz
