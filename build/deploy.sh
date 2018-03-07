3535b6be47f12f6b56a205804ecdfc391183f195
AkhilSudhakaran <akhil.sudhakaran@innovaturelabs.com>
2018-03-07 18:02:01 +0530

    deploy28

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,8 @@
 tar -zcvf test.tar.gz src/
-scp test.tar.gz sokonokado@52.66.73.199:/home/sokonokado/
+scp -o "StrictHostKeyChecking=no" test.tar.gz sokonokado@52.66.73.199:/home/sokonokado/
+# tar -zcvf test.tar.gz src/
+# scp test.tar.gz sokonokado@52.66.73.199:/home/sokonokado/
 #
 # #Here you extract your archive on EC2 using SSH
-# ssh sokonokado@52.66.73.199 tar -xvzf test.tar.gz
+ssh sokonokado@52.66.73.199 tar -xvzf test.tar.gz
 # - run: <command>
